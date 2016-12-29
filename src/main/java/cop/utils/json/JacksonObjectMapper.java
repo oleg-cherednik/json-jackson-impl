0f9094e5101f2d527e0c67c6cf54b96eab7c45dc
Oleg Cherednik <Oleg_Cherednik@epam.com>
2016-12-29 12:22:32 +0300

    #1 Add PrettyPrint option

--- src/main/java/cop/utils/json/JacksonObjectMapper.java
+++ src/main/java/cop/utils/json/JacksonObjectMapper.java
@@ -5,6 +5,7 @@ import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.PropertyAccessor;
 import com.fasterxml.jackson.core.Version;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.databind.module.SimpleModule;
 
 /**
@@ -15,22 +16,34 @@ final class JacksonObjectMapper extends ObjectMapper {
     private static final long serialVersionUID = 6353895542108257075L;
 
     private static final ThreadLocal<ObjectMapper> THREAD_LOCAL_MAPPER = new ThreadLocal<>();
+    private static final ThreadLocal<ObjectMapper> THREAD_LOCAL_PRETTY_MAPPER = new ThreadLocal<>();
 
     public static ObjectMapper getMapper() {
-        ObjectMapper mapper = THREAD_LOCAL_MAPPER.get();
+        return getMapper(false);
+    }
+
+    public static ObjectMapper getPrettyMapper() {
+        return getMapper(true);
+    }
+
+    private static ObjectMapper getMapper(boolean prettyPrint) {
+        ThreadLocal<ObjectMapper> threadLocal = prettyPrint ? THREAD_LOCAL_PRETTY_MAPPER : THREAD_LOCAL_MAPPER;
+        ObjectMapper mapper = threadLocal.get();
 
         if (mapper == null)
-            THREAD_LOCAL_MAPPER.set(mapper = new JacksonObjectMapper());
+            threadLocal.set(mapper = new JacksonObjectMapper(prettyPrint));
 
         return mapper;
     }
 
-    private JacksonObjectMapper() {
+    private JacksonObjectMapper(boolean prettyPrint) {
         registerModule(new SimpleModule(getClass().getSimpleName(), new Version(1, 0, 0, null, null, null)));
         setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.NONE);
         setVisibility(PropertyAccessor.FIELD, JsonAutoDetect.Visibility.ANY);
         setSerializationInclusion(JsonInclude.Include.NON_NULL);
 //        configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
-//        enable(SerializationFeature.INDENT_OUTPUT);
+
+        if (prettyPrint)
+            enable(SerializationFeature.INDENT_OUTPUT);
     }
 }
--- src/main/java/cop/utils/json/JsonUtils.java
+++ src/main/java/cop/utils/json/JsonUtils.java
@@ -51,6 +51,10 @@ public final class JsonUtils {
         JacksonObjectMapper.getMapper().writeValue(out, obj);
     }
 
+    public static <T> void writePrettyValue(T obj, OutputStream out) throws IOException {
+        JacksonObjectMapper.getPrettyMapper().writeValue(out, obj);
+    }
+
     private JsonUtils() {
     }
 }
