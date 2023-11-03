698e27e0824631f092d371c4a8c6fa90882f06e1
Oleg Cherednik <oleg.cherednik@gmail.com>
2023-11-03 23:12:52 +0300

    Update dependencies

---
+++ src/main/java/ru/olegcherednik/jackson_utils/JacksonUtilsHelper.java
@@ -0,0 +1,88 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package ru.olegcherednik.jackson_utils;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+
+import java.util.Optional;
+import java.util.function.Supplier;
+
+/**
+ * @author Oleg Cherednik
+ * @since 19.11.2014
+ */
+public final class JacksonUtilsHelper {
+
+    public static final Supplier<ObjectMapper> DEFAULT_BUILDER = JacksonObjectMapperSupplier.builder().build();
+
+    private static Supplier<ObjectMapper> mapperBuilder = DEFAULT_BUILDER;
+    private static ObjectMapper mapper = createMapper();
+    private static ObjectMapper prettyPrintMapper = createPrettyPrintMapper();
+
+    @SuppressWarnings("PMD.DefaultPackage")
+    static synchronized ObjectMapper mapper() {
+        return mapper;
+    }
+
+    @SuppressWarnings("PMD.DefaultPackage")
+    static synchronized ObjectMapper prettyPrintMapper() {
+        return prettyPrintMapper;
+    }
+
+    public static ObjectMapper createMapper() {
+        return mapperBuilder.get();
+    }
+
+    public static ObjectMapper createPrettyPrintMapper() {
+        return mapperBuilder.get().enable(SerializationFeature.INDENT_OUTPUT);
+    }
+
+    public static ObjectMapper createMapper(Supplier<ObjectMapper> mapperSupplier) {
+        return mapperSupplier.get();
+    }
+
+    public static ObjectMapper createPrettyPrintMapper(Supplier<ObjectMapper> mapperSupplier) {
+        return mapperSupplier.get().enable(SerializationFeature.INDENT_OUTPUT);
+    }
+
+    public static ObjectMapperDecorator createPrettyPrintMapperDecorator(Supplier<ObjectMapper> mapperSupplier) {
+        return new ObjectMapperDecorator(createPrettyPrintMapper(mapperSupplier));
+    }
+
+    public static ObjectMapperDecorator createMapperDecorator(Supplier<ObjectMapper> mapperSupplier) {
+        return new ObjectMapperDecorator(createMapper(mapperSupplier));
+    }
+
+    @SuppressWarnings({ "PMD.AvoidReassigningParameters", "PMD.CompareObjectsWithEquals" })
+    public static synchronized void setMapperBuilder(Supplier<ObjectMapper> mapperSupplier) {
+        mapperSupplier = Optional.ofNullable(mapperSupplier).orElse(DEFAULT_BUILDER);
+
+        if (mapperSupplier == JacksonUtilsHelper.mapperBuilder)
+            return;
+
+        JacksonUtilsHelper.mapperBuilder = mapperSupplier;
+        mapper = createMapper();
+        prettyPrintMapper = createPrettyPrintMapper();
+    }
+
+    private JacksonUtilsHelper() {
+    }
+
+}
---
+++ src/test/java/ru/olegcherednik/jackson_utils/WritePrettyPrintJacksonUtilsTest.java
@@ -0,0 +1,92 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package ru.olegcherednik.jackson_utils;
+
+import org.testng.annotations.Test;
+import ru.olegcherednik.jackson_utils.data.Data;
+
+import java.io.IOException;
+import java.io.StringWriter;
+import java.io.Writer;
+import java.util.List;
+import java.util.Map;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static ru.olegcherednik.jackson_utils.PrettyPrintUtils.UNIX_LINE_SEPARATOR;
+import static ru.olegcherednik.jackson_utils.PrettyPrintUtils.withUnixLineSeparator;
+
+/**
+ * @author Oleg Cherednik
+ * @since 07.01.2021
+ */
+@Test
+public class WritePrettyPrintJacksonUtilsTest {
+
+    public void shouldRetrievePrettyPrintJsonWhenWriteObjectWithPrettyPrint() {
+        Data data = new Data(555, "victory");
+        String actual = JacksonUtils.prettyPrint().writeValue(data);
+        assertThat(actual).isNotNull();
+        assertThat(withUnixLineSeparator(actual)).isEqualTo('{' + UNIX_LINE_SEPARATOR +
+                "  \"intVal\" : 555," + UNIX_LINE_SEPARATOR +
+                "  \"strVal\" : \"victory\"" + UNIX_LINE_SEPARATOR +
+                '}');
+    }
+
+    public void shouldRetrievePrettyPrintJsonWhenWriteMapObjectWithPrettyPrint() {
+        Map<String, Data> data = MapUtils.of(
+                "victory", new Data(555, "victory"),
+                "omen", new Data(666, "omen"));
+        String actual = JacksonUtils.prettyPrint().writeValue(data);
+        assertThat(withUnixLineSeparator(actual)).isEqualTo('{' + UNIX_LINE_SEPARATOR +
+                "  \"victory\" : {" + UNIX_LINE_SEPARATOR +
+                "    \"intVal\" : 555," + UNIX_LINE_SEPARATOR +
+                "    \"strVal\" : \"victory\"" + UNIX_LINE_SEPARATOR +
+                "  }," + UNIX_LINE_SEPARATOR +
+                "  \"omen\" : {" + UNIX_LINE_SEPARATOR +
+                "    \"intVal\" : 666," + UNIX_LINE_SEPARATOR +
+                "    \"strVal\" : \"omen\"" + UNIX_LINE_SEPARATOR +
+                "  }" + UNIX_LINE_SEPARATOR +
+                '}');
+    }
+
+    public void shouldRetrievePrettyPrintJsonWhenWriteListObjectWithPrettyPrint() {
+        List<Data> data = ListUtils.of(new Data(555, "victory"), new Data(666, "omen"));
+        String actual = JacksonUtils.prettyPrint().writeValue(data);
+        assertThat(actual).isNotNull();
+        assertThat(withUnixLineSeparator(actual)).isEqualTo("[ {" + UNIX_LINE_SEPARATOR +
+                "  \"intVal\" : 555," + UNIX_LINE_SEPARATOR +
+                "  \"strVal\" : \"victory\"" + UNIX_LINE_SEPARATOR +
+                "}, {" + UNIX_LINE_SEPARATOR +
+                "  \"intVal\" : 666," + UNIX_LINE_SEPARATOR +
+                "  \"strVal\" : \"omen\"" + UNIX_LINE_SEPARATOR +
+                "} ]");
+    }
+
+    public void shouldWritePrettyPrintJsonToStreamWhenWriteObjectWithPrettyPrintToStream() throws IOException {
+        try (Writer out = new StringWriter()) {
+            Data data = new Data(666, "omen");
+            JacksonUtils.prettyPrint().writeValue(data, out);
+            assertThat(withUnixLineSeparator(out.toString())).isEqualTo('{' + UNIX_LINE_SEPARATOR +
+                    "  \"intVal\" : 666," + UNIX_LINE_SEPARATOR +
+                    "  \"strVal\" : \"omen\"" + UNIX_LINE_SEPARATOR +
+                    '}');
+        }
+    }
+
+}
